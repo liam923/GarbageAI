250512f5af351f9c9b71a651256909c125680f19
Liam Stevenson <liam923@verizon.net>
2020-02-09 00:11:42 -0500

    Made gui functional

--- trashcan/main.py
+++ trashcan/main.py
@@ -1,7 +1,8 @@
 from trashcan import Trashcan
+from trash_type import all as all_trash_types, pretty
 import sys
 import process
-from guizero import App, Text
+from guizero import App, Text, Picture
 
 class Main():
     def __init__(self, trashcanID):
@@ -11,12 +12,17 @@ class Main():
         self.stats_widget = Text(self.app, "")
         self.update_stats()
 
+        self.detected_widget = Text(self.app, "")
+
+        self.display_qr(None)
+
         self.trashcan.listen(self.completion_handler)
         self.app.display()
 
     def update_stats(self):
         self.stats_widget.clear()
-        self.stats_widget.append("\n".join(["%s: %s" % i for i in self.trashcan.stats.items()]))
+        arr = ["%s: %s" % (pretty(k), v) for k, v in self.trashcan.stats.items()]
+        self.stats_widget.append("\n".join(arr + [""] * (len(all_trash_types) - len(arr))))
 
     def completion_handler(self, doc):
         data = doc.to_dict()
@@ -25,6 +31,15 @@ class Main():
         self.trashcan.handle_trash(trash_type)
         self.update_stats()
         self.trashcan.generate_qr_code(doc.id, trash_type, 'temp/qr.png')
+        self.display_qr(trash_type)
+
+    def display_qr(self, trash_type):
+        if hasattr(self, 'qr_widget'):
+            self.qr_widget.destroy()
+        self.detected_widget.clear()
+        if trash_type is not None:
+            self.detected_widget.append("Detected %s" % trash_type)
+            self.qr_widget = Picture(self.app, "temp/qr.png")
 
 if __name__ == '__main__':
     Main(sys.argv[1])
--- trashcan/trash_type.py
+++ trashcan/trash_type.py
@@ -5,8 +5,13 @@ GLASS = "glass"
 PLASTIC = "plastic"
 CARDBOARD = "cardboard"
 
+all = [TRASH, PAPER, METAL, GLASS, PLASTIC, CARDBOARD]
+
 def get_direction(trash_type):
     if trash_type == TRASH:
         return "left"
     else:
         return "right"
+
+def pretty(trash_type):
+    return trash_type.capitalize()
