ff6cca90946bc438919ff98b6483b49f58cd67b1
Liam Stevenson <liam923@verizon.net>
2020-02-09 06:04:03 -0500

    Support colored QR codes

--- ios-app/GarbageAI/ScannerViewController.swift
+++ ios-app/GarbageAI/ScannerViewController.swift
@@ -2,96 +2,37 @@
 
 import AVFoundation
 import UIKit
+import CodeScanner
 
-class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
-    var captureSession: AVCaptureSession!
-    var previewLayer: AVCaptureVideoPreviewLayer!
+class ScannerViewController: UIViewController {
+
+    private var scanner: CodeScanner!
 
     override func viewDidLoad() {
+
         super.viewDidLoad()
 
-        view.backgroundColor = UIColor.black
-        captureSession = AVCaptureSession()
+        self.navigationItem.title = "Detect QR code from Camera"
+        self.view.backgroundColor = .groupTableViewBackground
 
-        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
-        let videoInput: AVCaptureDeviceInput
-
-        do {
-            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
-        } catch {
-            return
-        }
-
-        if (captureSession.canAddInput(videoInput)) {
-            captureSession.addInput(videoInput)
-        } else {
-            failed()
-            return
-        }
-
-        let metadataOutput = AVCaptureMetadataOutput()
-
-        if (captureSession.canAddOutput(metadataOutput)) {
-            captureSession.addOutput(metadataOutput)
-
-            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-            metadataOutput.metadataObjectTypes = [.qr]
-        } else {
-            failed()
-            return
-        }
-
-        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        previewLayer.frame = view.layer.bounds
-        previewLayer.videoGravity = .resizeAspectFill
-        view.layer.addSublayer(previewLayer)
-
-        captureSession.startRunning()
+        self.scanner = CodeScanner(metadataObjectTypes: [AVMetadataObject.ObjectType.qr], preview: self.view)
     }
 
-    func failed() {
-        let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default))
-        present(ac, animated: true)
-        captureSession = nil
-    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-
-        if (captureSession?.isRunning == false) {
-            captureSession.startRunning()
+        CodeScanner.requestCameraPermission { (success) in
+            if success {
+                self.scanner.scan(resultOutputs: { (outputs) in
+                    print(outputs)
+                })
+            }
         }
-    }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-
-        if (captureSession?.isRunning == true) {
-            captureSession.stopRunning()
-        }
-    }
-
-    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
-        captureSession.stopRunning()
-
-        if let metadataObject = metadataObjects.first {
-            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
-            guard let stringValue = readableObject.stringValue else { return }
-            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
-            found(code: stringValue)
-        }
-
-        dismiss(animated: true)
     }
 
     func found(code: String) {
         let jsonData = code.data(using: .utf8) ?? Data()
         Database.shared.record(trash: try! JSONDecoder().decode(QRData.self, from: jsonData))
         self.navigationController?.popViewController(animated: true)
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        return .portrait
     }
 }
