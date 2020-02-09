5f9de00a87360e08e5a2ccf9c491970ae10f5fe3
Liam Stevenson <liam923@verizon.net>
2020-02-09 00:47:56 -0500

    afijn

--- trashcan/process.py
+++ trashcan/process.py
@@ -1,10 +1,16 @@
 from trash_type import *
 import urllib.request
 from pathlib import Path
+import random
+from PIL import Image
+from resizeimage import resizeimage
 
 def download(url, filename):
     Path("temp").mkdir(parents=True, exist_ok=True)
     urllib.request.urlretrieve(url, filename)
 
 def process(filename):
-    return PLASTIC
+    with open(filename, 'r+b') as f:
+        with Image.open(f) as image:
+            cover = resizeimage.resize_cover(image, [384, 512]).save(filename, image.format)
+    return random.choice(all)
